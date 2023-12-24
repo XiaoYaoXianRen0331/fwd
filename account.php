cfeba26858dc645dedf0587a47d42320b6498806
My Name <myemail@example.com>
2023-12-24 20:00:53 +0800

    midhomework登入系統

---
+++ account.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+session_start();
+$id = $_POST['login'];
+$password = $_POST['pass'];
+$level = $_POST['level'];
+
+// $link=mysqli_connect('localhost','root', '','test');
+$link = mysqli_connect('localhost', 'xiaoyao', 'xiaoyao', 'fwd');
+$sql = "select distinct * from account where id='$id' and password='$password' and level='$level'";
+$result = $link->query($sql);
+if ($row = mysqli_fetch_assoc($result)) {
+    $_SESSION['id'] = $id;
+    $_SESSION['name'] = $row['name'];
+    $_SESSION['level'] = $row['level'];
+    echo 'Welcome, redirecting...';
+    switch ($level) {
+        case 'admin':
+            $href = 'admin.html';
+            break;
+        case 'teacher':
+            $href = 'midhomeworkteacher_group.html';
+            break;
+        case 'student':
+            $href = 'student.html';
+            break;
+    }
+} else {
+    echo 'Login failed';
+    $href = 'midhomework.html';
+}
+?>
+</body>
+<script>
+    setTimeout(() => {
+        window.location.href = '<?php echo $href; ?>';
+    }, 1000);
+</script>
+</html>
--- login.php
+++ login.php
@@ -8,30 +8,24 @@
 <body>
 
     <?php
-    //要記得去php.ini搜尋display_error,把On改成Off。auto_start=0改成=1。_cookie_lifetime=0改成=9600
-    session_start();
-    $id=$_POST['id'];
-    $password=$_POST['password'];
-    $_SESSION['id']='';
-    $_SESSION['name']='';
-    $_SESSION['level']='';
+//要記得去php.ini搜尋display_error,把On改成Off。auto_start=0改成=1。_cookie_lifetime=0改成=9600
+session_start();
+$id = $_POST['id'];
+$password = $_POST['password'];
 
-    // $link=mysqli_connect('localhost','root', '','test');
-    $link = mysqli_connect('localhost','xiaoyao','xiaoyao','fwd');
-    $sql="select distinct * from account where id='$id' and password='$password'";
-    $result = $link->query($sql);
-    if($row=mysqli_fetch_assoc($result))
-        {
-           $_SESSION['id']=$id;
-           $_SESSION['name']=$row['name'];
-           $_SESSION['level']=$row['level']; 
-           header("Location:message.php?message=登入成功");
-        }
-    else
-        {
-            header("Location:message.php?message=查無此帳號與密碼");
-        }
-    ?>
-    
+// $link=mysqli_connect('localhost','root', '','test');
+$link = mysqli_connect('localhost', 'xiaoyao', 'xiaoyao', 'fwd');
+$sql = "select distinct * from account where id='$id' and password='$password'";
+$result = $link->query($sql);
+if ($row = mysqli_fetch_assoc($result)) {
+    $_SESSION['id'] = $id;
+    $_SESSION['name'] = $row['name'];
+    $_SESSION['level'] = $row['level'];
+    header("Location:message.php?message=登入成功");
+} else {
+    header("Location:message.php?message=查無此帳號與密碼");
+}
+?>
+
 </body>
 </html>
